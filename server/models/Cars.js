0aa3e5f860f01a4bb353df6266004e403c78dc55
piekloh <kk0708@o2.pl>
2022-08-07 15:46:59 +0200

    first commit

---
+++ server/models/Cars.js
@@ -0,0 +1,47 @@
+const {DataTypes} = require('sequelize');
+
+
+module.exports = (sequelize, DataTypes) => {
+
+  const Cars = sequelize.define("Cars", {
+    brand:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    model:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    passengers:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    doors:{
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    gearbox:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price:{
+      type: DataTypes.DOUBLE,
+      allowNull: false
+    },
+    size:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    available:{
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    image:{
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+
+
+  })
+  return Cars;
+}
