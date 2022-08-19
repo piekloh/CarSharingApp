3eba960153b2830987a0ea44e3166eb0a4871f45
piekloh <kk0708@o2.pl>
2022-08-19 21:09:47 +0200

    addingWithInsomnia

--- server/index.js
+++ server/index.js
@@ -1,53 +1,18 @@
 const express = require('express');
 const app = express();
 const db = require('./models');
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
+
+//Static folder
+app.use('/Images', express.static('../client/public/Images'))
+
 //Routers
 
 const carRouter = require('./routes/Cars');
 app.use('/cars', carRouter);
-
-
-
-
-
-
-
-// //image to db//
-// const multer = require('multer')
-// const path = require('path')
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb)=>{
-//     cb(null, '../client/Images')
-//   },
-//   filename: (req, file, cb)=>{
-//     cb(null, Date.now() + path.extname(file.originalname))
-//   }
-// })
-
-// const upload = multer({
-//   storage: storage,
-//   limits: {fileSize: '5000000'}, //5 mb(5 000 000 bajtów)
-//   fileFilter: (req, file, cb) =>{
-//     const fileTypes = /jpeg|jpg|png|gif/
-//     const mimeType = fileTypes.test(file.mimetype) //sprawdza, czy dobry format
-//     const extname = fileTypes.test(path.extname(file.originalname))
-
-//     if(mimeType && extname){
-//       return cb(null, true)
-//     }
-//     cb('Give proper files formate to upload') //może być potrzebne else
-//   }
-// }).single('image') //ten sam "image" co w models/Cars.js
-// //single, czyli, że będziemy dodawać tylko 1 obrazek
-
-
-// module.exports={
-//   upload
-// }
-// //image to db//
 
 
 
--- server/routes/Cars.js
+++ server/routes/Cars.js
@@ -2,13 +2,62 @@ const express = require('express');
 const router = express.Router();
 const {Cars} = require('../models')
 
+////////////////////image to db///////////////////
+const multer = require('multer')
+const path = require('path')
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb)=>{
+    cb(null, './../client/public/Images')
+  },
+  filename: (req, file, cb)=>{
+    cb(null, Date.now() + path.extname(file.originalname))
+  }
+})
+
+const upload = multer({
+  storage: storage,
+  limits: {fileSize: '5000000'}, //5 mb(5 000 000 bajtów)
+  fileFilter: (req, file, cb) =>{
+    const fileTypes = /jpeg|jpg|png|gif/
+    const mimeType = fileTypes.test(file.mimetype) //sprawdza, czy dobry format
+    const extname = fileTypes.test(path.extname(file.originalname))
+
+    if(mimeType && extname){
+      return cb(null, true)
+    }
+    cb('Give proper files formate to upload') //może być potrzebne else
+  }
+}).single('image') //ten sam "image" co w models/Cars.js(kolumna w BD)
+//single, czyli, że będziemy dodawać tylko 1 obrazek
+
+
+module.exports={
+  upload
+}
+////////////////////image to db////////////////////
+
+
+
+
+
 router.get('/', async (req, res)=>{
   const listOfCars = await Cars.findAll();
   res.json(listOfCars);
 });
 
-router.post('/', async (req,res)=>{
-  const car = req.body;
+router.post('/', upload,async (req,res)=>{
+  let car = {
+    brand: req.body.brand,
+    model: req.body.model,
+    passengers: req.body.passengers,
+    doors: req.body.doors,
+    gearbox: req.body.gearbox,
+    price: req.body.price,
+    size: req.body.size,
+    available: req.body.available,
+    image: req.file.path
+  }
   await Cars.create(car);
   res.json(car);
   //
