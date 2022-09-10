cec26a96e492bacf9c74ef1e37c145b735b26685
piekloh <kk0708@o2.pl>
2022-09-10 19:19:37 +0200

    dropdown menu and create page ready

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,18 @@ import "./App.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap';
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
-import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
-import LoginIcon from '@mui/icons-material/Login';
-import LogoutIcon from '@mui/icons-material/Logout';
+import PersonAddAltIcon from "@mui/icons-material/PersonAddAlt";
+import LoginIcon from "@mui/icons-material/Login";
+import LogoutIcon from "@mui/icons-material/Logout";
 import CreateCar from "./pages/CreateCar";
 import Car from "./pages/Car";
 import Login from "./pages/Login";
 import Registration from "./pages/Registration";
 import { AuthContext } from "./helpers/AuthContext";
-
 
 function App() {
   const [authState, setAuthState] = useState({
@@ -51,7 +51,6 @@ function App() {
       id: 0,
       status: false,
     });
-
   };
 
   return (
@@ -66,8 +65,8 @@ function App() {
             <button
               className="navbar-toggler"
               type="button"
-              data-toggle="collapse"
-              data-target="#navbarNavDropdown"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavDropdown"
               aria-controls="navbarNavDropdown"
               aria-expanded="false"
               aria-label="Toggle navigation"
@@ -76,7 +75,7 @@ function App() {
             </button>
             <div className="collapse navbar-collapse" id="navbarNavDropdown">
               <ul className="navbar-nav">
-                <li className="nav-item active">
+                <li className="nav-item active dropdown">
                   <Link to="/" className="nav-link" href="#">
                     Strona główna <span className="sr-only"></span>
                   </Link>
@@ -84,40 +83,42 @@ function App() {
               </ul>
               <form className="form-inline ms-auto ">
                 <ul className="navbar-nav">
-                  <li className="nav-item">
+                  <li className="nav-item dropdown">
                     <input
-                      className="form-control"
+                      className="form-control "
                       type="search"
                       placeholder="Wyszukaj na stronie..."
                       aria-label="Wyszukaj"
                     />
                   </li>
-                  <li className="nav-item">
-                    <button className="btn mx-2" type="submit">
+                  <li className="nav-item dropdown">
+                    <button className="btn searchButton" type="submit">
                       Szukaj
                     </button>
                   </li>
                   {!authState.status ? (
                     <>
-                      <li className="nav-item active">
+                      <li className="nav-item active dropdown">
                         <Link to="/login" className="nav-link" href="#">
-                          <LoginIcon/> Logowanie <span className="sr-only"></span>
+                          <LoginIcon /> Logowanie{" "}
+                          <span className="sr-only"></span>
                         </Link>
                       </li>
-                      <li className="nav-item active">
+                      <li className="nav-item active dropdown">
                         <Link to="/registration" className="nav-link" href="#">
-                          <PersonAddAltIcon/> Rejestracja <span className="sr-only"></span>
+                          <PersonAddAltIcon /> Rejestracja{" "}
+                          <span className="sr-only"></span>
                         </Link>
                       </li>
                     </>
                   ) : (
                     <>
-                      <li className="nav-item active">
-                        <Link to='/' className="nav-link" onClick={logout}> 
-                          <LogoutIcon/> Wyloguj
+                      <li className="nav-item active dropdown">
+                        <Link to="/" className="nav-link" onClick={logout}>
+                          <LogoutIcon /> Wyloguj
                         </Link>
                       </li>
-                      <li className="nav-item mx-2 loggedAsStatus">
+                      <li className="nav-item dropdown loggedAsStatus">
                         Jesteś zalogowany jako:
                         <div className="loggedAsUser">
                           <b>{authState.username}</b>
@@ -147,7 +148,9 @@ function App() {
 
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/createcar" element={<CreateCar />} />
+            {authState.username === "admin" && (
+              <Route path="/createcar" element={<CreateCar />} />
+            )}
             <Route path="/car/:id" element={<Car />} />
             <Route path="/login" element={<Login />} />
             <Route path="/registration" element={<Registration />} />
--- client/src/pages/CreateCar.js
+++ client/src/pages/CreateCar.js
@@ -1,164 +1,194 @@
-import React from 'react';
-import {Formik, Form, Field, ErrorMessage} from 'formik';
-import * as Yup from 'yup';
-import axios from 'axios';
-import {useNavigate} from 'react-router-dom'
-import CarBrands from '../helpers/CarBrands';
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import CarBrands from "../helpers/CarBrands";
 
-const FormData = require('form-data');
-
+const FormData = require("form-data");
 
 function CreateCar() {
-
   let navigate = useNavigate();
 
   const initialValues = {
-    brand: '',
-    model: '',
-    passengers: '',
-    doors: '',
-    gearbox: '',
-    price: '',
-    size: '',
-    available: '',
-    image:''
-  }
+    brand: "",
+    model: "",
+    passengers: "",
+    doors: "",
+    gearbox: "",
+    price: "",
+    size: "",
+    available: "",
+    image: "",
+  };
 
-  const validationSchema= Yup.object().shape({
+  const validationSchema = Yup.object().shape({
     brand: Yup.string().required("Wybierz markę"),
     model: Yup.string().required("Wybierz model"),
-    passengers: Yup.number().required('Podaj liczbę pasażerów'),
-    doors: Yup.number().required('Podaj liczbę drzwi'),
+    passengers: Yup.number().required("Podaj liczbę pasażerów"),
+    doors: Yup.number().required("Podaj liczbę drzwi"),
     gearbox: Yup.string().required("Wybierz rodzaj skrzyni biegów"),
-    price: Yup.number().typeError('Cena musi być liczbą').required("Podaj cenę"),
+    price: Yup.number()
+      .typeError("Cena musi być liczbą")
+      .required("Podaj cenę"),
     size: Yup.string().required("Podaj rozmiar"),
     available: Yup.string().required("Podaj dostępność"),
-    image:Yup.string().required("Wybierz zdjęcie"),
-  })
+    image: Yup.string().required("Wybierz zdjęcie"),
+  });
 
-  const onSubmit = (data)=>{
+  const onSubmit = (data) => {
     const form = new FormData();
 
-  //  console.log(data)
+    //  console.log(data)
 
-    form.append('brand', data.brand);
-    form.append('model', data.model);
-    form.append('passengers', data.passengers);
-    form.append('doors', data.doors);
-    form.append('gearbox', data.gearbox);
-    form.append('price', data.price);
-    form.append('size', data.size);
-    form.append('available', data.available);
-    form.append('image', data.image);
+    form.append("brand", data.brand);
+    form.append("model", data.model);
+    form.append("passengers", data.passengers);
+    form.append("doors", data.doors);
+    form.append("gearbox", data.gearbox);
+    form.append("price", data.price);
+    form.append("size", data.size);
+    form.append("available", data.available);
+    form.append("image", data.image);
 
-
-    axios.post("http://localhost:3001/cars", form, {headers: {accessToken: localStorage.getItem('accessToken')}})
-      .then((response)=>{
-        console.log(response.data)
-      navigate('/');
-    })
-    
-  }
+    axios
+      .post("http://localhost:3001/cars", form, {
+        headers: { accessToken: localStorage.getItem("accessToken") },
+      })
+      .then((response) => {
+        console.log(response.data);
+        navigate("/");
+      });
+  };
 
   return (
-    <div className='createCarPage'>
-      <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
-
-        {({values, setFieldValue})=>(
+    <div className="createCarPage">
+      <div className="createCarTitle">Dodaj nowy samochód</div>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        {({ values, setFieldValue }) => (
           <Form>
-{/* brand */}
-            <div className='inputField'>
-              <ErrorMessage name='brand' component='p'/>
+            {/* brand */}
+            <div className="inputField">
+              <ErrorMessage name="brand" component="p" />
               <label>Marka samochodu: </label>
-              <Field className='inputCreateCar' name='brand' as='select'>
-                <option value=''>-</option>
-                <CarBrands/>           
+              <Field className="inputCreateCar" name="brand" as="select">
+                <option value="">-</option>
+                <CarBrands />
               </Field>
             </div>
-{/* model */}
-            <div className='inputField'>
-            <ErrorMessage name='model' component='p'/>
+            {/* model */}
+            <div className="inputField">
+              <ErrorMessage name="model" component="p" />
               <label>Model: </label>
-              <Field className='inputCreateCar' name='model' placeholder='np. Corsa'/>
+              <Field
+                className="inputCreateCar"
+                name="model"
+                placeholder="np. Corsa"
+              />
             </div>
-{/* passengers */}            
-            <div className='inputField'>
-            <ErrorMessage name='passengers' component='p'/>
+            {/* passengers */}
+            <div className="inputField">
+              <ErrorMessage name="passengers" component="p" />
               <label>Liczba pasażerów: </label>
-              <Field className='inputCreateCar' name='passengers' as='select'>
-                <option value=''>-</option>
-                <option value='2'>2</option>
-                <option value='3'>3</option>
-                <option value='4'>4</option>
-                <option value='5'>5</option>
-                <option value='6'>6</option>
-                <option value='7'>7</option>
-                <option value='8'>8</option>
-                <option value='9'>9</option>
+              <Field className="inputCreateCar" name="passengers" as="select">
+                <option value="">-</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
               </Field>
             </div>
-{/* doors */}
-            <div className='inputField'>
-            <ErrorMessage name='doors' component='p'/>
+            {/* doors */}
+            <div className="inputField">
+              <ErrorMessage name="doors" component="p" />
               <label>Liczba drzwi: </label>
-              <Field className='inputCreateCar' name='doors' as='select'>
-                <option value='' type='number'>-</option>
-                <option value='3' type='number'>3</option>
-                <option value='5' type='number'>5</option>
+              <Field className="inputCreateCar" name="doors" as="select">
+                <option value="" type="number">
+                  -
+                </option>
+                <option value="3" type="number">
+                  3
+                </option>
+                <option value="5" type="number">
+                  5
+                </option>
               </Field>
             </div>
-{/* gearbox */}
-            <div className='inputField'>
-            <ErrorMessage name='gearbox' component='p'/>
+            {/* gearbox */}
+            <div className="inputField">
+              <ErrorMessage name="gearbox" component="p" />
               <label>Skrzynia biegów: </label>
-              <Field className='inputCreateCar' name='gearbox' as='select'>
-                <option value=''>-</option>
-                <option value='manualna'>Manualna</option>
-                <option value='automatyczna'>Automatyczna</option>
+              <Field className="inputCreateCar" name="gearbox" as="select">
+                <option value="">-</option>
+                <option value="manualna">Manualna</option>
+                <option value="automatyczna">Automatyczna</option>
               </Field>
             </div>
-{/* price */}
-            <div className='inputField'>
-            <ErrorMessage name='price' component='p'/>
+            {/* price */}
+            <div className="inputField">
+              <ErrorMessage name="price" component="p" />
               <label>Cena za dobę w zł: </label>
-              <Field className='inputCreateCar' name='price' placeholder='np. 400'/></div>
-{/* size */}
-            <div className='inputField'>
-            <ErrorMessage name='size' component='p'/>
+              <Field
+                className="inputCreateCar"
+                name="price"
+                placeholder="np. 400"
+              />
+            </div>
+            {/* size */}
+            <div className="inputField">
+              <ErrorMessage name="size" component="p" />
               <label>Rozmiar: </label>
-              <Field className='inputCreateCar' name='size' as='select'>
-                <option value=''>-</option>
-                <option value='mały'>Mały</option>
-                <option value='średni'>Średni</option>
-                <option value='duży'>Duży</option>
+              <Field className="inputCreateCar" name="size" as="select">
+                <option value="">-</option>
+                <option value="mały">Mały</option>
+                <option value="średni">Średni</option>
+                <option value="duży">Duży</option>
               </Field>
             </div>
-{/* available */}
-            <div className='inputField'>
-            <ErrorMessage name='available' component='p'/>
+            {/* available */}
+            <div className="inputField">
+              <ErrorMessage name="available" component="p" />
               <label>Dostępność: </label>
-              <Field className='inputCreateCar' name='available' as='select' type='boolean'>
-                <option value=''>-</option>
-                <option value='true'>Dostępny</option>
-                <option value='false'>Niedostępny</option>
+              <Field
+                className="inputCreateCar"
+                name="available"
+                as="select"
+                type="boolean"
+              >
+                <option value="">-</option>
+                <option value="true">Dostępny</option>
+                <option value="false">Niedostępny</option>
               </Field>
             </div>
-{/* image */}
-            <div className='inputField'>
-            <ErrorMessage name='image' component='p'/>
+            {/* image */}
+            <div className="inputField">
+              <ErrorMessage name="image" component="p" />
               <label>Zdjęcie: </label>
-              <input className='inputCreateCar' name='image' type='file' onChange={(event)=>{setFieldValue('image', event.target.files[0])}} />
+              <input
+                className="inputCreateCar"
+                id="inputImage"
+                name="image"
+                type="file"
+                onChange={(event) => {
+                  setFieldValue("image", event.target.files[0]);
+                }}
+              />
             </div>
 
-            <button type='submit'>
-              Dodaj samochód
-            </button>
+            <button type="submit">Dodaj samochód</button>
           </Form>
         )}
-        
       </Formik>
     </div>
-  )
+  );
 }
 
-export default CreateCar
+export default CreateCar;
