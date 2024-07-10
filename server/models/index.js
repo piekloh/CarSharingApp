1d2f88fe26172cc8dc04aa4134be7ab5db3b29a3
Krzysztof Kukielka <Krzysztof.Kukielka@proaxia-consulting.com>
2024-07-10 14:41:37 +0200

    configuration moved to env variables

--- server/models/index.js
+++ server/models/index.js
@@ -4,29 +4,19 @@ const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
+const dbConfig = require("../config/config.js");
 const db = {};
 
-//configuration for local DB:
-// "development": {
-//   "username": "root",
-//   "password": "root",
-//   "database": "carsharingdb",
-//   "host": "localhost",
-//   "dialect": "mysql"
-// },
-
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, {
-    host: config.host,
-    dialect: config.dialect,
-    port: config.port,
-  });
-}
+let sequelize = new Sequelize(
+  dbConfig.database,
+  dbConfig.username,
+  dbConfig.password,
+  {
+    host: dbConfig.host,
+    dialect: dbConfig.dialect,
+    port: dbConfig.port,
+  }
+);
 
 sequelize
   .authenticate()
