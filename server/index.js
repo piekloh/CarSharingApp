2150702acb67fd49a63d1c1d36c01a70ca5af51a
Krzysztof Kukielka <Krzysztof.Kukielka@proaxia-consulting.com>
2024-07-11 09:12:22 +0200

    Images folder name changed

--- server/index.js
+++ server/index.js
@@ -1,29 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const db = require('./models');
-const cors = require('cors');
+const db = require("./models");
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 
 //Static folder
-app.use('/images', express.static('../client/public/images'))
+app.use("/images", express.static("../client/public/images"));
 
 //Routers
 
-const carRouter = require('./routes/Cars');
-app.use('/cars', carRouter);
-const opinionRouter = require('./routes/Opinions');
-app.use('/opinions', opinionRouter);
-const userRouter = require('./routes/Users');
-app.use('/auth', userRouter);
-const reservationRouter = require('./routes/Reservations');
-app.use('/reservations', reservationRouter);
+const carRouter = require("./routes/Cars");
+app.use("/cars", carRouter);
+const opinionRouter = require("./routes/Opinions");
+app.use("/opinions", opinionRouter);
+const userRouter = require("./routes/Users");
+app.use("/auth", userRouter);
+const reservationRouter = require("./routes/Reservations");
+app.use("/reservations", reservationRouter);
 
-
-
-db.sequelize.sync().then(()=>{
+db.sequelize.sync().then(() => {
   app.listen(3001, () => {
-    console.log("server running on port 3001")
+    console.log("server running on port 3001");
   });
 });
